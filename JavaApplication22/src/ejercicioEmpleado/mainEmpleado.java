dc25563295300647d5307a578412af1606ca4151
Celia9728 <cml0034@alu.medac.es>
2025-04-02 11:49:40 +0200

    feat: meter main

---
+++ JavaApplication22/src/ejercicioEmpleado/mainEmpleado.java
@@ -0,0 +1,9 @@
+
+package ejercicioEmpleado;
+
+public class mainEmpleado {
+
+    public static void main(String[] args) {
+    }
+    
+}
---
+++ JavaApplication22/src/ejercicioProductos/mainProductos.java
@@ -0,0 +1,9 @@
+
+package ejercicioProductos;
+
+public class mainProductos {
+
+    public static void main(String[] args) {
+    }
+    
+}
